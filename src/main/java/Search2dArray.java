a51bb4cb00d05b1d454dd8cb71af7fe98c74c183
Michelle Mendez <mmendez5584@gmail.com>
2023-03-03 10:11:46 +0000

    Search 2D Array Lab

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,19 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; i < nums.length; j++){
+                if(nums[i][j] == target){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+
         return false;
     }
 }
